c5723466768b6459f2528a98263240050672f6cf
vectorCodes <ansarihassan0703@gmail.com>
2022-01-23 12:28:43 +0530

    fixes margin

--- components/goals.js
+++ components/goals.js
@@ -5,7 +5,7 @@ function Goals() {
   return (
     <Box>
       <SimpleGrid
-        columns={{ base: 1, sm: 1, md: 2, lg: 2, xl: 2 }}
+        columns={{ base: 1, sm: 2, md: 2, lg: 2, xl: 2 }}
         spacing={{ base: 6, sm: 20, md: 10, lg: 12, xl: 20 }}
         m={"0 auto"}
         maxW={"7xl"}
@@ -21,7 +21,7 @@ function Goals() {
                 lg: "3xl",
                 xl: "5xl",
               }}
-              ml={{ base: 20, sm: 40, md: 14, lg: 14, xl: 10 }}
+              ml={{ base: 10, sm: 4, md: 14, lg: 14, xl: 10 }}
             >
               Your Goals Matter To Us
             </Text>
@@ -34,14 +34,13 @@ function Goals() {
                 xl: "3xl",
               }}
               mt={{ base: 5, sm: 4, md: 0, lg: 0, xl: 0 }}
-              ml={{ base: 0, sm: 0, md: 14, lg: 14, xl: 10 }}
-              textAlign={"center"}
+              ml={{ base: 10, sm: 4, md: 14, lg: 14, xl: 10 }}
             >
               learn as you earn
             </Text>
             <Text
               mt={{ base: 3, sm: 5, md: 6, lg: 8, xl: 10 }}
-              ml={{ base: 10, sm: 0, md: 14, lg: 14, xl: 10 }}
+              ml={{ base: 10, sm: 4, md: 14, lg: 14, xl: 10 }}
               fontSize={{
                 base: "md",
                 sm: "xl",
@@ -49,7 +48,6 @@ function Goals() {
                 lg: "2xl",
                 xl: "3xl",
               }}
-              textAlign={"center"}
             >
               We provide the best training for Market & Analysis to grow your
               income.
@@ -60,12 +58,12 @@ function Goals() {
           <Image
             w={{
               base: "300px",
-              sm: "350px",
+              sm: "330px",
               md: "400px",
               lg: "500px",
               xl: "600px",
             }}
-            ml={{ base: 28, sm: 36, md: 0, lg: 0, xl: 0 }}
+            ml={{ base: 10, sm: 0, md: 0, lg: 0, xl: 0 }}
             src="/assest/computer.png"
             alt="computer"
           />
