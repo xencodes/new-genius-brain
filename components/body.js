ff136069a814475b564dba57b0228e737f1715d5
xencodes <xencodes@icloud.com>
2022-06-09 07:56:04 +0530

    fixes

--- components/body.js
+++ components/body.js
@@ -1,44 +1,50 @@
-import { Box, Button, Center, Text } from "@chakra-ui/react";
-import React from "react";
+import { Box, Button, Center, Text } from '@chakra-ui/react';
+import React from 'react';
 
 function Body() {
-  return (
-    <Box>
-      <Text
-        fontWeight={"bold"}
-        fontSize={{ base: "xl", sm: "lg", md: "2xl", lg: "4xl", xl: "5xl" }}
-        textAlign={"center"}
-      >
-        Did We Mention...
-      </Text>
-      <Text
-        textAlign={"center"}
-        fontSize={{ base: "md", sm: "lg", md: "2xl", lg: "2xl", xl: "3xl" }}
-        mt={{ base: 4, sm: 6, md: 8, lg: 10, xl: 12 }}
-        px={{ base: 2, sm: 6, md: 5, lg: 8, xl: 0 }}
-      >
-        You can join our free program to learn how to trade and make money in
-        the FOREX Market
-      </Text>
-      <Text
-        textAlign={"center"}
-        fontSize={{ base: "md", sm: "lg", md: "2xl", lg: "2xl", xl: "3xl" }}
-        mt={{ base: 2, sm: 4, md: 6, lg: 8, xl: 10 }}
-        px={{ base: 2, sm: 6, md: 5, lg: 8, xl: 0 }}
-      >
-        Our team hosts bi-weekly webinars and send Trading Signals chat!
-      </Text>
-      <Center mt={{ base: 8, sm: 14, md: 20, lg: 24, xl: 14 }}>
-        <Button
-          w={{ base: 40, sm: 40, md: 40, lg: 40, xl: 40 }}
-          fontWeight={"bold"}
-          fontSize={"xl"}
-        >
-          JOIN US
-        </Button>
-      </Center>
-    </Box>
-  );
+	return (
+		<Box>
+			<Text
+				fontWeight={'bold'}
+				fontSize={{ base: 'xl', sm: 'lg', md: '2xl', lg: '4xl', xl: '5xl' }}
+				textAlign={'center'}
+			>
+				Did We Mention...
+			</Text>
+			<Text
+				textAlign={'center'}
+				fontSize={{ base: 'md', sm: 'lg', md: '2xl', lg: '2xl', xl: '3xl' }}
+				mt={{ base: 4, sm: 6, md: 8, lg: 10, xl: 12 }}
+				px={{ base: 2, sm: 6, md: 5, lg: 8, xl: 0 }}
+			>
+				You can join our free program to learn how to trade and make money in
+				the FOREX Market
+			</Text>
+			<Text
+				textAlign={'center'}
+				fontSize={{ base: 'md', sm: 'lg', md: '2xl', lg: '2xl', xl: '3xl' }}
+				mt={{ base: 2, sm: 4, md: 6, lg: 8, xl: 10 }}
+				px={{ base: 2, sm: 6, md: 5, lg: 8, xl: 0 }}
+			>
+				Our team hosts bi-weekly webinars and send Trading Signals chat!
+			</Text>
+			<Center mt={{ base: 8, sm: 14, md: 20, lg: 24, xl: 14 }}>
+				<Button
+					w={{ base: 40, sm: 40, md: 40, lg: 40, xl: 40 }}
+					fontWeight={'bold'}
+					fontSize={'xl'}
+				>
+					<a
+						href='https://t.me/Geniussignalbot'
+						target={'_blank'}
+						rel='noreferrer'
+					>
+						Join Us
+					</a>
+				</Button>
+			</Center>
+		</Box>
+	);
 }
 
 export default Body;
--- components/deals.js
+++ components/deals.js
@@ -1,95 +1,73 @@
 import {
-  Box,
-  Button,
-  ListItem,
-  SimpleGrid,
-  Text,
-  UnorderedList,
-  VStack,
-} from "@chakra-ui/react";
-import React from "react";
+	Box,
+	Button,
+	ListItem,
+	SimpleGrid,
+	Text,
+	UnorderedList,
+	VStack,
+} from '@chakra-ui/react';
+import React from 'react';
 
 function Deals() {
-  return (
-    <Box>
-      <Text
-        fontSize={{ base: "2xl", sm: "2xl", md: "3xl", lg: "4xl", xl: "5xl" }}
-        textAlign={"center"}
-        fontWeight={"bold"}
-      >
-        Deals
-      </Text>
-      <SimpleGrid
-        columns={{ base: 1, sm: 2, md: 2, lg: 2, xl: 2 }}
-        m={"0 auto"}
-        maxW={"6xl"}
-        spacing={{ base: 4, sm: 4, md: 6, lg: 6, xl: 0 }}
-      >
-        <VStack mt={{ base: 6, sm: 8, md: 12, lg: 14, xl: 16 }}>
-          <Text
-            fontWeight={"bold"}
-            fontSize={{ base: "lg", sm: "xl", md: "2xl", lg: "2xl", xl: "3xl" }}
-          >
-            PREMIUM PROGRAM
-          </Text>
-          <Box>
-            <Text
-              fontWeight={"medium"}
-              fontSize={{ base: "lg", sm: "lg", md: "xl", lg: "xl", xl: "2xl" }}
-            >
-              Starting at $130 /mo
-            </Text>
-            <UnorderedList
-              fontSize={{ base: "md", sm: "lg", md: "xl", lg: "xl", xl: "xl" }}
-            >
-              <ListItem>Webinars</ListItem>
-              <ListItem>Daily Trading Signals</ListItem>
-              <ListItem>Tips</ListItem>
-              <ListItem>Analysis</ListItem>
-              <ListItem>Chat with Our Team</ListItem>
-              <ListItem>Start a Demo Account</ListItem>
-              <ListItem>Lifetime Mentorship</ListItem>
-            </UnorderedList>
-          </Box>
-          <Box>
-            <cardElement />
-            <Button colorScheme={"blue"}>Buy now</Button>
-          </Box>
-        </VStack>
-        <VStack mt={{ base: 6, sm: 8, md: 12, lg: 14, xl: 16 }}>
-          <Text
-            fontWeight={"bold"}
-            fontSize={{ base: "lg", sm: "xl", md: "2xl", lg: "2xl", xl: "3xl" }}
-          >
-            PREMIUM PROGRAM(2)
-          </Text>
-          <Box>
-            <Text
-              fontWeight={"medium"}
-              fontSize={{ base: "lg", sm: "lg", md: "xl", lg: "xl", xl: "2xl" }}
-            >
-              Starting at $160 /mo
-            </Text>
-            <UnorderedList
-              fontSize={{ base: "md", sm: "lg", md: "xl", lg: "xl", xl: "xl" }}
-            >
-              <ListItem>Risk Management</ListItem>
-              <ListItem>30% Profit Split</ListItem>
-              <ListItem>Bot Assistant</ListItem>
-              <ListItem>Trading Experts</ListItem>
-              <ListItem>Channels</ListItem>
-              <ListItem>Mentorships</ListItem>
-              <ListItem>Analysis</ListItem>
-              <ListItem>Signals</ListItem>
-            </UnorderedList>
-          </Box>
-          <Box>
-            <Button colorScheme={"blue"}>Buy now</Button>
-          </Box>
-        </VStack>
-      </SimpleGrid>
-    </Box>
-  );
+	return (
+		<Box>
+			<Text
+				fontSize={{ base: '2xl', sm: '2xl', md: '3xl', lg: '4xl', xl: '5xl' }}
+				textAlign={'center'}
+				fontWeight={'bold'}
+			>
+				Deals
+			</Text>
+			<SimpleGrid
+				columns={{ base: 1, sm: 2, md: 2, lg: 2, xl: 2 }}
+				m={'0 auto'}
+				maxW={'6xl'}
+				spacing={{ base: 4, sm: 4, md: 6, lg: 6, xl: 0 }}
+			>
+				<VStack mt={{ base: 6, sm: 8, md: 12, lg: 14, xl: 16 }}>
+					<Text
+						fontWeight={'bold'}
+						fontSize={{ base: 'lg', sm: 'xl', md: '2xl', lg: '2xl', xl: '3xl' }}
+					>
+						PREMIUM PROGRAM
+					</Text>
+					<Box>
+						<Text
+							fontWeight={'medium'}
+							fontSize={{ base: 'lg', sm: 'lg', md: 'xl', lg: 'xl', xl: '2xl' }}
+						>
+							Starting at $130 /mo
+						</Text>
+						<UnorderedList
+							fontSize={{ base: 'md', sm: 'lg', md: 'xl', lg: 'xl', xl: 'xl' }}
+						>
+							<ListItem>Webinars</ListItem>
+							<ListItem>Daily Trading Signals</ListItem>
+							<ListItem>Tips</ListItem>
+							<ListItem>Analysis</ListItem>
+							<ListItem>Chat with Our Team</ListItem>
+							<ListItem>Start a Demo Account</ListItem>
+							<ListItem>Lifetime Mentorship</ListItem>
+						</UnorderedList>
+					</Box>
+					<Box>
+						<cardElement />
+						<Button colorScheme={'blue'}>
+							{' '}
+							<a
+								href='https://t.me/Geniussignalbot'
+								target={'_blank'}
+								rel='noreferrer'
+							>
+								Buy Now
+							</a>
+						</Button>
+					</Box>
+				</VStack>
+			</SimpleGrid>
+		</Box>
+	);
 }
 
 export default Deals;
